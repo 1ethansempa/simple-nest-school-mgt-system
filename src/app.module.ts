cc22009e60fe559007d50744682f8b1cbf1fdcb1
1ethansempa <esempa@sailglobalcorp.com>
2023-10-03 10:16:29 +0300

    fix: env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
+import 'dotenv/config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: `${process.env.MONGO_CONNECTION_STRING}`,
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
